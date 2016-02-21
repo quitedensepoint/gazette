c91cd3c02c3479ef73792ff6c352c665ce0cb3cb
Jason Rout <drloon@playnet.com>
2016-02-21 17:57:08 +1100

    Added Destroyer library types

---
+++ lib/Playnet/WwiiOnline/WwiiOnline/Models/Vehicle/Types/Destroyer/German.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Copyright Playnet 2016
+ */
+
+namespace Playnet\WwiiOnline\WwiiOnline\Models\Vehicle\Types\Destroyer;
+
+use Playnet\WwiiOnline\WwiiOnline\Models\CountryInterface;
+use Playnet\WwiiOnline\WwiiOnline\Models\Vehicle\Types\TypeInterface;
+use Playnet\WwiiOnline\WwiiOnline\Models\Vehicle\Types\TypeTrait;
+use Playnet\WwiiOnline\WwiiOnline\Traits\German as GermanTrait;
+use Playnet\WwiiOnline\WwiiOnline\Models\Vehicle\Classes\Sea;
+
+/**
+ * Represents a German Destroyer Unit
+ *
+ * @author Jason "drloon" Rout
+ */
+class German extends Sea implements CountryInterface, TypeInterface{
+	
+	/** Ensures this class can access side and country identifiers */
+	use GermanTrait, TypeTrait;
+	
+	/** The ID of the vehicle in wwii_vehtype */
+	const OBJECT_ID = 80;
+	
+	protected static $typeId = 1;
+
+}
