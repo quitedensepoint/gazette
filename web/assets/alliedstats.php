b344957a732dc642a62efcd2e3efba7ac61caca4
sniper62 <sniper62@playnet.com>
2016-06-16 08:16:19 -0400

    #863-Adds the changes that B2k did to correct a few stats issues on the Allied and Axis pages

--- web/assets/alliedstats.php
+++ web/assets/alliedstats.php
@@ -13,8 +13,9 @@ $tatg =  mysqli_query($dbconn, "SELECT atgcallsign, atgpoints FROM scoring_top_p
 $tatr =  mysqli_query($dbconn, "SELECT atrcallsign, atrpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY atrpoints DESC LIMIT 1") or die ($dbconn->error.tatr_LINE_);
 $tbomb =  mysqli_query($dbconn, "SELECT bombcallsign, bombpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY bombpoints DESC LIMIT 1") or die ($dbconn->error.tcapstreak_LINE_);
 $tfight =  mysqli_query($dbconn, "SELECT fightcallsign, fightpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY fightpoints DESC LIMIT 1") or die ($dbconn->error.tcapstreak_LINE_);
-$tdd =  mysqli_query($dbconn, "SELECT ddcallsign, ddpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY ddpoints DESC LIMIT 1") or die ($dbconn->error.tdd_LINE_);
-$tpb =  mysqli_query($dbconn, "SELECT pbcallsign, pbpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY pbpoints DESC LIMIT 1") or die ($dbconn->error.tpb_LINE_);
+$tdd =  mysqli_query($dbconn, "SELECT ddcallsign, ddpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY ddpoints DESC LIMIT 1") ;
+$tpb =  mysqli_query($dbconn, "SELECT pbcallsign, pbpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY pbpoints DESC LIMIT 1") ;
+$ttt =  mysqli_query($dbconn, "SELECT ttcallsign, ttpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY ttpoints DESC LIMIT 1") ;
 $tsmg =  mysqli_query($dbconn, "SELECT smgcallsign, smgpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY smgpoints DESC LIMIT 1") or die ($dbconn->error.smg_LINE_);
 $tlmg =  mysqli_query($dbconn, "SELECT lmgcallsign, lmgpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY lmgpoints DESC LIMIT 1") or die ($dbconn->error.lmg_LINE_);
 $ttank =  mysqli_query($dbconn, "SELECT tankcallsign, tankpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY tankpoints DESC LIMIT 1") or die ($dbconn->error.tanker_LINE_);
@@ -23,7 +24,6 @@ $tgren =  mysqli_query($dbconn, "SELECT grencallsign, grenpoints FROM scoring_to
 $teng =  mysqli_query($dbconn, "SELECT engcallsign, engpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY engpoints DESC LIMIT 1") or die ($dbconn->error.eng_LINE_);
 $tmort =  mysqli_query($dbconn, "SELECT mortcallsign, mortpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY mortpoints DESC LIMIT 1") or die ($dbconn->error.mort_LINE_);
 $ttruck =  mysqli_query($dbconn, "SELECT truckcallsign,truckpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY truckpoints DESC LIMIT 1") or die ($dbconn->error.truck_LINE_);
-$ttt =  mysqli_query($dbconn, "SELECT ttcallsign, ttpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY ttpoints DESC LIMIT 1") or die ($dbconn->error.tt_LINE_);
 $tkrow =  mysqli_query($dbconn, "SELECT krowcallsign, krowpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY krowpoints DESC LIMIT 1") or die ($dbconn->error.krow_LINE_);
 $tcrow =  mysqli_query($dbconn, "SELECT crowcallsign, crowpoints FROM scoring_top_players WHERE side ='1' AND period = 'camp' ORDER BY crowpoints DESC LIMIT 1") or die ($dbconn->error.tt_LINE_);
 
@@ -40,8 +40,9 @@ $datg =  mysqli_query($dbconn, "SELECT atgcallsign, atgpoints FROM scoring_top_p
 $datr =  mysqli_query($dbconn, "SELECT atrcallsign, atrpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY atrpoints DESC LIMIT 1") or die ($dbconn->error.tatr_LINE_);
 $dbomb =  mysqli_query($dbconn, "SELECT bombcallsign, bombpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY bombpoints DESC LIMIT 1") or die ($dbconn->error.tcapstreak_LINE_);
 $dfight =  mysqli_query($dbconn, "SELECT fightcallsign, fightpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY fightpoints DESC LIMIT 1") or die ($dbconn->error.tcapstreak_LINE_);
-$ddd =  mysqli_query($dbconn, "SELECT ddcallsign, ddpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY ddpoints DESC LIMIT 1") or die ($dbconn->error.tdd_LINE_);
-$dpb =  mysqli_query($dbconn, "SELECT pbcallsign, pbpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY pbpoints DESC LIMIT 1") or die ($dbconn->error.tpb_LINE_);
+$ddd =  mysqli_query($dbconn, "SELECT ddcallsign, ddpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY ddpoints DESC LIMIT 1") ;
+$dpb =  mysqli_query($dbconn, "SELECT pbcallsign, pbpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY pbpoints DESC LIMIT 1") ;
+$dtt =  mysqli_query($dbconn, "SELECT ttcallsign, ttpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY ttpoints DESC LIMIT 1") ;
 $dsmg =  mysqli_query($dbconn, "SELECT smgcallsign, smgpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY smgpoints DESC LIMIT 1") or die ($dbconn->error.smg_LINE_);
 $dlmg =  mysqli_query($dbconn, "SELECT lmgcallsign, lmgpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY lmgpoints DESC LIMIT 1") or die ($dbconn->error.lmg_LINE_);
 $dtank =  mysqli_query($dbconn, "SELECT tankcallsign, tankpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY tankpoints DESC LIMIT 1") or die ($dbconn->error.tanker_LINE_);
@@ -50,7 +51,6 @@ $dgren =  mysqli_query($dbconn, "SELECT grencallsign, grenpoints FROM scoring_to
 $deng =  mysqli_query($dbconn, "SELECT engcallsign, engpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY engpoints DESC LIMIT 1") or die ($dbconn->error.eng_LINE_);
 $dmort =  mysqli_query($dbconn, "SELECT mortcallsign, mortpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY mortpoints DESC LIMIT 1") or die ($dbconn->error.mort_LINE_);
 $dtruck =  mysqli_query($dbconn, "SELECT truckcallsign,truckpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY truckpoints DESC LIMIT 1") or die ($dbconn->error.truck_LINE_);
-$dtt =  mysqli_query($dbconn, "SELECT ttcallsign, ttpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY ttpoints DESC LIMIT 1") or die ($dbconn->error.tt_LINE_);
 $dkrow =  mysqli_query($dbconn, "SELECT krowcallsign, krowpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY krowpoints DESC LIMIT 1") or die ($dbconn->error.krow_LINE_);
 $dcrow =  mysqli_query($dbconn, "SELECT crowcallsign, crowpoints FROM scoring_top_players WHERE side ='1' AND period = 'day' ORDER BY crowpoints DESC LIMIT 1") or die ($dbconn->error.tt_LINE_);
 ?>
